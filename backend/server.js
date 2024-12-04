a1ebedce40e49b3659a7b88f22d8502079209963
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-12-04 14:29:16 +0100

    feat: Setup und Grundstruktur: Node.js/Express Backend aufsetzen(backend);  frontend- vite React project

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// Express importieren
+import express from "express";
+const app = express();
+
+// damit express die daten aus dem body auswertet
+//app.use(express.urlencoded());
+app.use(express.json());
+
+// Eine einfache Route
+app.get('/', (req, res) => {
+    res.send('Hallo, Welt!');
+});
+
+// Server starten
+app.listen(3000, () => {
+    console.log('Server läuft auf http://localhost:3000');
+});
