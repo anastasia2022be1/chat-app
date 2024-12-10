eec0893ee94bacc64642e3b3ca12ea9ed510b7e5
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-12-10 10:12:29 +0100

    feat: add dependencies-cors, express, mongoose; connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-// Express importieren
 import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+
+await mongoose.connect(process.env.MONGODB_DB)
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 const app = express();
 
 // damit express die daten aus dem body auswertet
-//app.use(express.urlencoded());
 app.use(express.json());
 
 // Eine einfache Route
