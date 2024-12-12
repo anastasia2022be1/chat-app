a5fe6365d32da5c4bbc856c479d75d3bab3f65e3
dci-student <yasaman.nikpay@gmail.com>
2024-12-12 13:45:17 +0100

    work on setting-frontend

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,16 +1,15 @@
-import UserSettings from "../components/UserSettings.jsx";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const handleSettingsClick = () => {
-        navigate('/settings');
-    };
+  const navigate = useNavigate();
+
+  // Navigate to settings page when the button is clicked
+  const handleSettingsClick = () => {
+    navigate("/setting");
+  };
   return (
     <header className="flex items-center justify-between bg-blue-600 text-white p-4 shadow-md">
-      <button onClick={handleSettingsClick}>
-        <UserSettings />
-      </button>
+      <button onClick={handleSettingsClick}>Setting</button>
 
       <p className="text-xl font-bold tracking-wide">LOGO</p>
     </header>
