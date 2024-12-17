7f1f73e2157948d7d0df0b9ba601e380d6ddc44a
Arian-Study <aristudy237@gmail.com>
2024-12-17 12:09:28 +0100

    socket io implementiert

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,20 +1,41 @@
+import { useState } from "react";
+
 // Message Component
-const Message = () => {
+function Message({socket}) {
+  const [message, setMessage] = useState([])
+  
+  const handleSend = (e) => {
+    e.preventDefault();
+    // console.log({
+    //   user: localStorage.getItem("username"),
+    //   message,
+    // });
+    // if(message.trim() && localStorage.getItem('username')) {
+    //   // with emit we send the message to the server
+
+      socket.emit("message", {
+        text: message,
+        id: `${socket.id}-${Math.random()}`,
+        socketID: socket.id
+      });
+    }
   return (
-    <div className="flex items-center p-4 bg-gray-100 border-t border-gray-300">
+    <form onSubmit={handleSend} className="flex items-center p-4 bg-gray-100 border-t border-gray-300">
       {/* Input Field */}
       <input
         type="text"
         placeholder="Type your message..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value) }
         className="flex-grow p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mr-3"
       />
 
       {/* Send Button */}
-      <button className="p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
+      <button type="submit" className="p-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
         Send
       </button>
-    </div>
+    </form>
   );
-};
+  }
 
 export default Message;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
-
 const Sidebar = () => {
-  const [modus, setModus] = useState("Contacts")
-
-  
-  
   return (
     <aside className="w-64 h-80 bg-gray-100  flex flex-col p-4 border-r border-gray-300 z-10">
 
-      <div className="contacts">
-              <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition mb-4">
+      <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition mb-4">
         Search
       </button>
-      <div>
-        <button>
-          Chats
-        </button>
-        <button>
-          Contacts
-        </button>
-      </div>
-    {
-
-      modus?
-    }
+    
       <ul className="space-y-2">
         <li className="p-2 bg-white rounded-md shadow-sm hover:bg-blue-100 transition cursor-pointer">
           Contact-1
@@ -45,11 +27,6 @@ const Sidebar = () => {
       <button className="mt-auto w-full bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 transition">
         Add new contact
       </button>
-      </div>
-      
-      <div className="chats">
-        <button>Chat</button>
-      </div>
     </aside>
   );
 };
