82e620c70a7b629dcfd484e2ab18531b845af247
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2025-01-08 15:05:48 +0100

    fix my server.js

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,6 @@ const io = new SocketIOServer(server, {
   },
 });
 
-// Function to get the receiver socket ID based on `receiverId`
-// export const getRecieverSocketId = (receiverId) => {
-//   return socketIds[receiverId];  
-// };
-
-
 // Middleware zur JSON-Parsierung
 app.use(express.json());
 app.use(cors());
@@ -52,8 +46,7 @@ io.on('connection', (socket) => {
       });
 
       await Chat.findByIdAndUpdate(chatId, { $push: { messages: newMessage._id } });
-      
-     // send the message to the specific chat room
+
       io.to(chatId).emit('message', newMessage);
     } catch (error) {
       console.log('Error: ', error);
