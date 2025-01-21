a3649254a8ae69abc65d848b6025a3f458979d66
Anastasiia Sevastianova <sevastyanova.anastasia1@gmail.com>
2025-01-21 10:44:18 +0100

    merge with yasaman branch

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -3,9 +3,9 @@ import { deleteMessage, getMessages } from "../controllers/messageController.js"
 
 const router = express.Router();
 
-// router.post("/message", createMessage);
 router.get('/message/:chatId', getMessages);
-//  router.get('/message/:chatId',  authenticate, getMessages);
-//   router.post("/message/:chatId", authenticate, createMessage );
+
+// DELETE a message
+router.delete("/message/:messageId", deleteMessage)
 
 export default router;
