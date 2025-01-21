6c959956b1165110dd991f4545a36fc5db6366ff
Anastasiia Sevastianova <sevastyanova.anastasia1@gmail.com>
2025-01-21 10:32:16 +0100

    delete logout function

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -7,42 +7,19 @@ export function AuthProvider({ children }) {
 
   useEffect(() => {
     const token = localStorage.getItem("authToken");
-    const userId = localStorage.getItem("userId");
-    if (token && userId) {
-      setAuthState({ token, userId });
-    }
-
-    // Flag in sessionStorage that keeps track of the current session
-    if (!sessionStorage.getItem("isSession")) {
-      sessionStorage.setItem("isSession", "true");
-
-      // Event handler before closing a window/tab
-      const handleBeforeUnload = () => {
-        // УDeleting data from localStorage when closing a window or tab
-        localStorage.removeItem("authToken");
-        localStorage.removeItem("userId");
-      };
-
-      // Adding an event handler
-      window.addEventListener("beforeunload", handleBeforeUnload);
-
-      // Clearing a handler when unmounting a component
-      return () => {
-        window.removeEventListener("beforeunload", handleBeforeUnload);
-      };
+    if (token) {
+      setAuthState({ token });
     }
   }, []);
 
   const login = (userData) => {
     setAuthState(userData);
     localStorage.setItem("authToken", userData.token);
-    localStorage.setItem("userId", userData.userId);
   };
 
   const logout = () => {
     setAuthState(null);
     localStorage.removeItem("authToken");
-    localStorage.removeItem("userId");
   };
 
   return (
