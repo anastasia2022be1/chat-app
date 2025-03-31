8a08eb62bab4708851935dda569470c2a8485c5c
Anastasiia Sevastianova <sevastyanova.anastasia1@gmail.com>
2025-03-31 12:54:55 +0200

    try to use ethereal

--- backend/controllers/emailService.js
+++ backend/controllers/emailService.js
@@ -7,7 +7,7 @@ console.log("📨 SMTP login:", process.env.BREVO_EMAIL);
 console.log("🔐 SMTP password (shortened):", process.env.BREVO_SMTP_KEY?.slice(0, 8));
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
+  host: "smtp.ethereal.email",
   port: 587,
   secure: false,
   auth: {
