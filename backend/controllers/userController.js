3f6f087dfdcb25ddd2a0e116262e59785ffef9af
Anastasiia Sevastianova <sevastyanova.anastasia1@gmail.com>
2025-03-31 11:52:45 +0200

    check brevo sending emails

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,7 @@ export const registerUser = async (req, res) => {
     // Send a verification email to the user with the verification token link
     const emailResponse = await transporter.sendMail({
       from: `"Talki.dev" <${process.env.BREVO_EMAIL}>`,
-      to: user.email, 
+      to: email, 
       subject:
         "Willkommen bei Talki! Bitte bestätigen Sie Ihre E-Mail-Adresse",
       html: `
